7e8fea5b19240ce80dd26381bd2158d00016a2e6
Daniel Isenmann <daniel.isenmann@gmail.com>
2021-01-14 01:22:22 +0100

    First draft of overview and click handling of a cover

--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -1,25 +1,62 @@
-import React, { Component } from 'react';
+import React, { Component, useCallback } from 'react';
+import AudioPlayer from 'react-h5-audio-player';
+import 'react-h5-audio-player/lib/styles.css';
+import Gallery from 'react-photo-gallery';
 
 export class Home extends Component {
   static displayName = Home.name;
 
+  constructor(props) {
+    super(props);
+    this.state = { albums: [], loading: true };
+  }
+
+  componentDidMount() {
+    this.populateAlbumData();
+  }
+
+  async populateAlbumData() {
+    const response = await fetch('Album/Overview');
+    const data = await response.json();
+    const covers = [];
+    let i = 0;
+
+    data.albums.forEach(element => {
+      let coverSrc = "placeholder.png";
+      if(element.coverId > 0)
+      {
+        coverSrc = `/Cover/${element.coverId}/front?scaled=true`;
+      }
+
+      covers.push({
+        key: (i++).toString(),
+        src: coverSrc,
+        width: 1,
+        height: 1,
+        coverId: element.coverId,
+        albumId: element.albumId
+      })
+    });
+
+    this.setState({ albums: covers, loading: false });
+  }
+
+  openCoverModal(albumId, coverId) {
+debugger
+  }
+
   render () {
     return (
       <div>
-        <h1>Hello, world!</h1>
-        <p>Welcome to your new single-page application, built with:</p>
-        <ul>
-          <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
-          <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
-          <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
-        </ul>
-        <p>To help you get started, we have also set up:</p>
-        <ul>
-          <li><strong>Client-side navigation</strong>. For example, click <em>Counter</em> then <em>Back</em> to return here.</li>
-          <li><strong>Development server integration</strong>. In development mode, the development server from <code>create-react-app</code> runs in the background automatically, so your client-side resources are dynamically built on demand and the page refreshes when you modify any file.</li>
-          <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
-        </ul>
-        <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
+        {/* <AudioPlayer
+          autoPlay
+          src="https://localhost:5001/Track/201"
+          onPlay={e => console.log("onPlay")}
+          // other props here
+        /> */}
+
+        <Gallery photos={this.state.albums} onClick={(event, photo) => {this.openCoverModal(photo.photo.albumId, photo.photo.coverId)}} />;
+       
       </div>
     );
   }
--- ClientApp/src/components/Layout.js
+++ ClientApp/src/components/Layout.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import { Container } from 'reactstrap';
-import { NavMenu } from './NavMenu';
 
 export class Layout extends Component {
   static displayName = Layout.name;
@@ -8,10 +6,7 @@ export class Layout extends Component {
   render () {
     return (
       <div>
-        <NavMenu />
-        <Container>
           {this.props.children}
-        </Container>
       </div>
     );
   }
